2dab1f04bcdac773a40e0c85288610d0a8983e64
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-05-01 13:16:18 +0300

    .

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import Lib
-import System.IO
+import System.IO ( hFlush, stdout )
 import Control.Monad (unless)
-import Control.Concurrent
+import Control.Concurrent ( threadDelay )
 import Data.List (sort, (\\))
 import Consul
 
@@ -16,58 +16,69 @@ main = do
       let init' = if null init then mcgv else sort init
       if not (isValid init') 
        then putStrLn "impossible"
-       else showTraffic (makeTrafficSt init') 
+       else drowTraffic (makeTrafficSt init')  
       main)
 
-showTraffic :: [(State, Boat)] -> IO ()
-showTraffic traffic = do
+drowTraffic :: [(State, Boat)] -> IO ()
+drowTraffic traffic = do
    putStr hideCur
    putStr clrscr
-   mapM_ showRoute traffic
+   mapM_ drowRoute traffic
    putStrLn showCur
 
-(bL, bR) = (5, 44) 
+(bL, bR) = (4, 34) 
 
 --          ("_cgv", ">_g")
-showRoute :: (State, Boat) -> IO()
-showRoute (state, boat) = do
+drowRoute :: (State, Boat) -> IO()
+drowRoute (state, boat) = do
    -- перед погрузкой
-   drawGoods onLeft onRight 
-   showBoat "_" dir (if dir == '>' then bL else bR)
+   drawBanks onLeft onRight 
+   drowBoat "_" dir (if dir == '>' then bL else bR)
    pause
    -- после погрузки
-   drawGoods (onLeft \\ load) (onRight \\ load)
-   showBoat load dir (if dir == '>' then bL else bR)
+   drawBanks (onLeft \\ load) (onRight \\ load)
+   drowBoat load dir (if dir == '>' then bL else bR)
    pause
    -- проход лодки
-   mapM_ (showBoat load dir) dist
+   mapM_ (drowBoat load dir) dist
    -- перед разгрузкой
    pause
    -- после разгрузки
    if dir == '<' 
-    then drawGoods (onLeft ++ load) (onRight \\ load)
-    else drawGoods (onLeft \\ load) (onRight ++ load)
-   showBoat "_" dir (if dir == '<' then bL else bR)
-
+    then drawBanks (onLeft ++ load) (onRight \\ load)
+    else drawBanks (onLeft \\ load) (onRight ++ load)
+   drowBoat "_" dir (if dir == '<' then bL else bR)
  where
-   (dir : _ : load) = boat
-   drawGoods l r = putStr $ 
-      rc 1 1  ++ take 5 (l ++ "      ") ++
-      rc 1 (bR + 6) ++ take 5 (r ++ "      ")  
+   (dir : _ : load) = boat  
    pause =  threadDelay 500000   
    onLeft = state \\ [man]
    onRight = goods \\ onLeft
    dist = if dir == '>' then [bL..bR] else [bR,(pred bR)..bL]
    
 
-showBoat :: 
+drowBoat :: 
    String    -- load
    -> Char   -- dir: '>' , '<' 
    -> Int    -- x-coord
    -> IO ()
-showBoat load dir col  = do
+drowBoat load dir col  = do
    let d = if dir == '>' then -1 else 1
-   putStr $ rc 1 (col + d) ++ "     " ++ rc 1 col ++ "\\___/" ++ back 3 ++ load
+   putStr $ rc 1 (col + d) ++ "     " ++ rc 1 col ++ "\\___/" ++ back 3 ++ showColored load
    hFlush stdout
    threadDelay 50000
-    
+
+drawBanks l r = putStr $ showColored $ l' ++ r'
+ where
+   l' = rc 1 1        ++ take 5 (l ++ "      ")
+   r' = rc 1 (bR + 6) ++ take 5 (r ++ "      ") 
+   
+
+showColored cs = concatMap showC cs ++ norm
+ where
+   showC 'c' = green ++ "c"
+   showC 'g' = blue ++ "g"
+   showC 'v' = red ++ "v"
+   showC x = [x]
+
+
+   
